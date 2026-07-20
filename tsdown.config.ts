3f69ae5a309ebde56e77aae1b0772c9cdce405c0
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2026-07-20 18:07:11 +0800

    v1.2.2: enable iife

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: {
+    cjs: {},
+    esm: {},
+    iife: {
+      deps: {
+        onlyBundle: ["blueimp-md5"],
+        alwaysBundle() {
+          return true;
+        },
+        dts: {
+          alwaysBundle() {
+            return true;
+          },
+        },
+        skipNodeModulesBundle: false,
+      },
+      platform: "browser",
+    },
+  },
   dts: true,
   minify: true,
   globalName: "ccwRequest",
